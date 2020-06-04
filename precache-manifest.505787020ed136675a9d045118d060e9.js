03fce141f14334328c2071d562c9778508532586
Arcalise08 <demonata08@gmail.com>
2020-06-04 18:31:39 -0500

    Updates

---
+++ precache-manifest.505787020ed136675a9d045118d060e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbaaef3a13640b88d39a1f0cc5aab202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f27a65718cd1004630e1",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5a29ec5d1cd0f96fc812",
+    "url": "/static/css/main.8d668f6d.chunk.css"
+  },
+  {
+    "revision": "f27a65718cd1004630e1",
+    "url": "/static/js/2.1ff70250.chunk.js"
+  },
+  {
+    "revision": "a5f655d830d088aa9894c35a94634b59",
+    "url": "/static/js/2.1ff70250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a29ec5d1cd0f96fc812",
+    "url": "/static/js/main.ae6d4046.chunk.js"
+  },
+  {
+    "revision": "84ac65994d53f35e12bc",
+    "url": "/static/js/runtime-main.82de50f5.js"
+  },
+  {
+    "revision": "ab41dc77577b94310abf34a39624bdda",
+    "url": "/static/media/clipart.ab41dc77.png"
+  },
+  {
+    "revision": "60c317b5a593dd7c03be541d966f7e4f",
+    "url": "/static/media/profile.60c317b5.jpg"
+  }
+]);
