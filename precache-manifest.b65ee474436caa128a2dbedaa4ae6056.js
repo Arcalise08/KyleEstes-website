3f69ca062b08e1aa7d3cb2a9498e3e5b42f1a354
Arcalise08 <demonata08@gmail.com>
2020-06-04 19:12:55 -0500

    Updates

---
+++ precache-manifest.b65ee474436caa128a2dbedaa4ae6056.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd20f041c1569026e4eeb78b60b06985",
+    "url": "/KyleEstes/index.html"
+  },
+  {
+    "revision": "195fda0d73420f2d0725",
+    "url": "/KyleEstes/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "90a7df61aa3e0f0086dd",
+    "url": "/KyleEstes/static/css/main.8d668f6d.chunk.css"
+  },
+  {
+    "revision": "195fda0d73420f2d0725",
+    "url": "/KyleEstes/static/js/2.0e6f790c.chunk.js"
+  },
+  {
+    "revision": "a5f655d830d088aa9894c35a94634b59",
+    "url": "/KyleEstes/static/js/2.0e6f790c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a7df61aa3e0f0086dd",
+    "url": "/KyleEstes/static/js/main.0c785a0d.chunk.js"
+  },
+  {
+    "revision": "478317d90940a3961ebc",
+    "url": "/KyleEstes/static/js/runtime-main.12d40468.js"
+  },
+  {
+    "revision": "ab41dc77577b94310abf34a39624bdda",
+    "url": "/KyleEstes/static/media/clipart.ab41dc77.png"
+  },
+  {
+    "revision": "60c317b5a593dd7c03be541d966f7e4f",
+    "url": "/KyleEstes/static/media/profile.60c317b5.jpg"
+  }
+]);
