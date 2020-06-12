58d7c581306fe1e403d8fba57a6b6b3a2f311a77
Arcalise08 <demonata08@gmail.com>
2020-06-11 20:20:58 -0500

    Updates

---
+++ precache-manifest.1f25a04c3e7931219a7b4c91439f4c10.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b7f7489f1129866b4ae924a5767f2c",
+    "url": "/KyleEstes-website/index.html"
+  },
+  {
+    "revision": "9e05dbc3394d1aa2455d",
+    "url": "/KyleEstes-website/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "174653e5ac0ff2c23011",
+    "url": "/KyleEstes-website/static/css/main.d376c21d.chunk.css"
+  },
+  {
+    "revision": "9e05dbc3394d1aa2455d",
+    "url": "/KyleEstes-website/static/js/2.74063606.chunk.js"
+  },
+  {
+    "revision": "a5f655d830d088aa9894c35a94634b59",
+    "url": "/KyleEstes-website/static/js/2.74063606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174653e5ac0ff2c23011",
+    "url": "/KyleEstes-website/static/js/main.c8b2e8d9.chunk.js"
+  },
+  {
+    "revision": "b2e30a5066abeac60de3",
+    "url": "/KyleEstes-website/static/js/runtime-main.5785877c.js"
+  },
+  {
+    "revision": "c48b2cee79cbb4603a915a86b6a3ae18",
+    "url": "/KyleEstes-website/static/media/KatChat.c48b2cee.jpg"
+  },
+  {
+    "revision": "d30c4a0ade8e0cff721078d829fe5e81",
+    "url": "/KyleEstes-website/static/media/My-Portfolio-Website.d30c4a0a.jpg"
+  },
+  {
+    "revision": "ab41dc77577b94310abf34a39624bdda",
+    "url": "/KyleEstes-website/static/media/clipart.ab41dc77.png"
+  },
+  {
+    "revision": "4ee778be3074adb880720bd6afb9c942",
+    "url": "/KyleEstes-website/static/media/dnd-app.4ee778be.jpg"
+  },
+  {
+    "revision": "adecd662eb7716c8ee73649a384a7be3",
+    "url": "/KyleEstes-website/static/media/games-app.adecd662.jpg"
+  },
+  {
+    "revision": "bbae5ae29540b75ca6dcfefba946174d",
+    "url": "/KyleEstes-website/static/media/meetup-app.bbae5ae2.jpg"
+  },
+  {
+    "revision": "1b948bbed3004e8002f950396adfe4a4",
+    "url": "/KyleEstes-website/static/media/my-quiz-app.1b948bbe.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/KyleEstes-website/precache-manifest.eeb098408f311149cd7d3c9768edd2a0.js"
+  "/KyleEstes-website/precache-manifest.1f25a04c3e7931219a7b4c91439f4c10.js"
 );
 
 self.addEventListener('message', (event) => {
